12ea20aedd1ca29235678241761fe5ea473e3e3d
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-02-16 00:47:11 +0100

    Add the category view and template

--- app.py
+++ app.py
@@ -44,6 +44,28 @@ def view_post(type, day, month, year, name):
     """ Returns a post identified by its name above """
     filename = "{}-{}-{}-{}.mkd".format(month,day,year,name)
 
+    # type of content determination
+    if type == 'p': type='post'
+    else: type='breve'
+
+    # source file fetching & processing
+    try:
+        source_file = open("{}s/{}".format(type,filename),'r')
+    except IOError: # means source_file does not exists
+        raise HTTPError(404, output="The post you've requested does not exist")
+    meta_brut = []
+    line = source_file.readline()
+    while line != "\n":
+        meta_brut.append(line)
+        line = source_file.readline()
+    meta = yaml.load(''.join(meta_brut))
+    text = markdown(re.sub(r'~\n', '', source_file.read()))
+    source_file.close()
+   
+    # output
+    return template('templates/{}.html'.format(type), text=text, meta=meta)
+
+
 
 def main():
     """ Run the application
